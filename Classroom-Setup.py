6a3da109bf88f0d28f3a1124e0f45d9e54f64f2f
Ben Sadeghi <ben.sadeghi1@gmail.com>
2019-09-30 09:52:56 +0800

    update setup

--- Classroom-Setup.py
+++ Classroom-Setup.py
@@ -2,8 +2,9 @@
 import re
 # Get user name
 userName = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+userName = re.sub(r"[^a-zA-Z0-9]", "_", userName)
 # Create database name
-databaseName = re.sub(r"[^a-zA-Z0-9]", "_", userName) + "_dbp"
+databaseName = userName + "_db"
 # Create database
 spark.sql("CREATE DATABASE IF NOT EXISTS `{}`".format(databaseName))
 # Use database
