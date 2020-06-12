56c37941c9cdf4b8a92f5443ee47b6c154753f58
Ben Sadeghi <ben.sadeghi1@gmail.com>
2020-06-12 15:30:15 +0800

    update 07

--- scripts/07-Delete-Resources.py
+++ scripts/07-Delete-Resources.py
@@ -11,8 +11,13 @@
 
 # COMMAND ----------
 
-# Remove data and model files, including the created directory
-dbutils.fs.rm(userName, recurse=True)
+# MAGIC %md
+# MAGIC ### Delete Files, Tables and Database
+
+# COMMAND ----------
+
+# Remove data file, including the created directory
+dbutils.fs.rm('dbfs:/Users/' + userName, recurse=True)
 
 # COMMAND ----------
 
@@ -28,8 +33,34 @@ spark.sql("DROP DATABASE `{}`".format(databaseName))
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Delete Registered Models
+
+# COMMAND ----------
+
+from mlflow.tracking.client import MlflowClient
+client = MlflowClient()
+
+modelName = "Titanic-Model__" + userName
+models = client.search_model_versions("name='{}'".format(modelName))
+
+# loop over registered models
+for i in range(len(models)):
+  try:
+    # set model stage to Archive
+    client.transition_model_version_stage(name=modelName, version=models[i].version, stage='Archived')
+  except:
+    pass
+  # delete version of model
+  client.delete_model_version(modelName, models[i].version)
+
+# delete model
+client.delete_registered_model(modelName)
+
+# COMMAND ----------
+
 # MAGIC %md-sandbox
-# MAGIC &copy; 2019 Databricks, Inc. All rights reserved.<br/>
+# MAGIC &copy; 2020 Databricks, Inc. All rights reserved.<br/>
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
 # MAGIC <br/>
 # MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
